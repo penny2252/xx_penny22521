deeec6edeb15dab6bc5843bd433ec58f018b31f3
penny2252 <18643112252@163.com>
2019-03-10 21:30:51 +0800

    nothing

--- xx_0306.py
+++ xx_0306.py
@@ -1,13 +1,16 @@
 class Abc(object):
     isinstance=None
     def __new__(cls, *args, **kwargs):
-        cls.isinstance=super().__new__(cls)
+        if cls.isinstance==None:
+            cls.isinstance=super().__new__(cls)
+
         return cls.isinstance
     # def __init__(self):
     #     print('a')
 
 
 a = Abc()
-print(a)
 b = Abc()
+print(a)
+
 print(b)
