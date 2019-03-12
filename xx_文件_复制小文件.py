e2403658ba78a8e54ba063b52ae197ea1373c879
penny2252 <18643112252@163.com>
2019-03-12 20:51:31 +0800

    nothing

--- xx_文件_复制小文件.py
+++ xx_文件_复制小文件.py
@@ -6,3 +6,16 @@ file2.close()
 file3=open('readme_1')
 print(file3.read())
 file3.close()
+#大文件
+file_read=open('readme')
+file_write=open('readme_2','w')
+while True:
+    txt=file_read.readline()
+    file_write.write(txt)
+    if txt=='':
+        break
+file_read.close()
+file_write.close()
+file3=open('readme_2')
+print(file3.read())
+file3.close()
